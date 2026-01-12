a66e5fcf4873384b2cd3f489bb49ee442179bafc
M4m1nuclcl1n_2401537009 <marufaminuddin7009@mhs.unisbank.ac.id>
2026-01-12 14:49:27 +0700

    Delete siap-siswa directory

--- siap-siswa/add_relationship_column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE student_parent_relations ADD COLUMN relationship_type ENUM('ayah', 'ibu', 'wali', 'kakek', 'nenek', 'saudara') NOT NULL DEFAULT 'ayah' AFTER parent_id;
--- siap-siswa/frontend/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'http://localhost/siap-siswa/backend/api.php'
-})
-
-export default instance
